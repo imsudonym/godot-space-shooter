e2469b8523e14c9f540e01d09fb0e581734e4f6d
Marie Edcel Balite <mdrbalite@gmail.com>
2022-04-26 12:20:20 +0800

    Initial commit

---
+++ StartMenu.gd
@@ -0,0 +1,8 @@
+extends Node
+
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		# warning-ignore:return_value_discarded
+		get_tree().change_scene("res://World.tscn")
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
